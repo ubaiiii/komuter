5d3459192567536f2305a7d1a0e520dbdeaf75ca
Ubaid <ubaiiii@gmail.com>
2023-07-20 15:40:09 +0800

    update on interface and depart time

--- main.py
+++ main.py
@@ -2,42 +2,72 @@ import pandas as pd
 import numpy as np
 import streamlit as st
 from datetime import datetime
+from datetime import time
 
 df1 = pd.read_excel('BC-PS-BC-Hari-Bekerja-10072023.xlsx', sheet_name="Table 1")
 df2 = pd.read_excel('BC-PS-BC-Hari-Bekerja-10072023.xlsx', sheet_name="Table 2")
 
+masa = datetime.now().time().strftime("%H:%M")
 
-with st.form(key='my_form'):
-    departure = st.selectbox("Depart from", options=df1["Nombor Tren"].unique(), index=0)
-    destination = st.selectbox("Destination from", options=df1["Nombor Tren"].unique(), index=1)
-    submit_button = st.form_submit_button(label='Submit')
+form = st.form("my_form")
+
+
+c = st.container()
+
+
+
+c.departure = form.selectbox("Depart from", options=df1["Nombor Tren"].unique(), index=0)
+c.destination = form.selectbox("Destination from", options=df1["Nombor Tren"].unique(), index=1)
+
+c.current_time = st.checkbox('Tick this :blue[checkbox] to check for different time :sunglasses:')
+
+if c.current_time:    
+        time_depart = st.slider( 
+            "Choose your departure time :",
+            value=(time(12, 00) ) )
+else:
+        time_depart = datetime.strptime((masa), '%H:%M').time() 
+
+form.form_submit_button("Submit")
+
+
+
+#st.write("You're scheduled for:", time_depart)
 
 # lookup schedule
 def trip_schedule(df_x, table):
-    masa = datetime.now().time().strftime("%H:%M")
-    now = datetime.strptime((masa), '%H:%M').time() 
+    
+ #   st.write("Time > ", time_depart)
 
      # check destination route if nan, drop col
-    list_station = [ departure, destination]
+    list_station = [ c.departure, c.destination]
     train_schedule = df_x.loc[df_x['Nombor Tren'].isin(list_station)]
     if ( table == "Table 1") :
         another_id = 1
+  #      st.write("Table 1 : ", another_id)
     else:
         another_id = 0
+   #     st.write("Table 2 : ", another_id)
 
     idx = 0
     column_to_be_drop = []
     for y in train_schedule:
         try:
-            if ( datetime.strptime((train_schedule.iat[another_id,idx]), '%H:%M').time() ) < now:
+            if ( datetime.strptime((train_schedule.iat[another_id,idx]), '%H:%M').time() ) < time_depart:
                 column_to_be_drop.append(idx)
             else:
                 datetime.strptime((train_schedule.iat[another_id,idx]), '%H:%M').time()
+                if another_id == 1:
+                    if ( datetime.strptime((train_schedule.iat[0,idx]), '%H:%M').time() ) < time_depart:
+                        column_to_be_drop.append(idx)
+#                        st.write("Dopped some more data", train_schedule[0,idx])
         except:
             column_to_be_drop.append(idx)
         
-        idx = idx + 1
+        #idx = idx + 1
+        idx += 1
     train_schedule.drop(train_schedule.columns[column_to_be_drop], axis=1, inplace=True) 
+    
     return train_schedule
 
 
@@ -48,9 +78,9 @@ y = 99
 table = None
 
 for index, row in df1.iterrows():
-    if row['Nombor Tren'] == departure:
+    if row['Nombor Tren'] == c.departure:
         x = index    
-    if row['Nombor Tren'] == destination:
+    if row['Nombor Tren'] == c.destination:
         y = index
 
     if ( y > x ) :
@@ -68,7 +98,7 @@ train_schedule = trip_schedule(df, table)
 ## formatting dataframe
 schedule_tranpose = train_schedule.T
 
-schedule_tranpose.columns = [departure, destination]
+schedule_tranpose.columns = [c.departure, c.destination]
 
 # CSS to inject contained in a string
 hide_table_row_index = """
